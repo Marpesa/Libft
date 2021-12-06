871adaebe5e4fc1df41d8458a3c5aae78f5a84c3
Louise MERY <lmery@e3r12p16.42.fr>
2021-12-06 14:45:06 +0100

    add_back_ses_morts

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmery <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/06 11:32:59 by lmery             #+#    #+#             */
-/*   Updated: 2021/12/06 11:33:18 by lmery            ###   ########.fr       */
+/*   Created: 2021/12/06 11:55:34 by lmery             #+#    #+#             */
+/*   Updated: 2021/12/06 14:34:53 by lmery            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **alst, t_list *new)
+void	ft_lstadd_back(t_list **alst, t_list *new)
 {
 	t_list	*temp;
 
@@ -23,8 +23,6 @@ void ft_lstadd_back(t_list **alst, t_list *new)
 	}
 	if (!new)
 		return ;
-	while (*alst->next != NULL)
-		*alst = *alst->next;
-	temp = *alst;
+	temp = ft_lstlast(*alst);
 	temp->next = new;
 }
